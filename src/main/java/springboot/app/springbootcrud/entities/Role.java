6f2a9d40d233fe4bcdf5aa3a4d42751c8ba7f611
haitao <haitaowu14@gmail.com>
2024-02-22 11:58:20 +0100

    JWT

---
+++ src/main/java/springboot/app/springbootcrud/entities/Role.java
@@ -0,0 +1,10 @@
+package springboot.app.springbootcrud.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "roles" )
+public class Role {
+
+}
